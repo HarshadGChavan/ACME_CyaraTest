ee712368d0c34f3b4817274f0c9767e2b84ba6e5
harshadgchavan <63164159+HarshadGChavan@users.noreply.github.com>
2021-08-29 08:54:12 +0530

    Token Manager Cotroller updated

--- ACME_Token_Mgmt/ACME-Token-Mgmt-FrontEnd/src/app/token-manager/token-manager.component.ts
+++ ACME_Token_Mgmt/ACME-Token-Mgmt-FrontEnd/src/app/token-manager/token-manager.component.ts
@@ -23,8 +23,7 @@ export class TokenManagerComponent implements AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
 
   constructor() {
-    // Create 100 users
-    //const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
     const users = [
       {No: 1, token: 'Hydrogen', expiry: "10 Aug 2021", active: true},
       {No: 2, token: 'Helium', expiry: "10 Aug 2021", active: true},
@@ -57,16 +56,3 @@ export class TokenManagerComponent implements AfterViewInit {
     alert(token);
   }
 }
-
-/** Builds and returns a new User. */
-// function createNewUser(No: number): UserData {
-//   const token = tokenS[Math.round(Math.random() * (tokenS.length - 1))] + ' ' +
-//     tokenS[Math.round(Math.random() * (tokenS.length - 1))].charAt(0) + '.';
-
-//   return {
-//     No: No.toString(),
-//     token: token,
-//     progress: Math.round(Math.random() * 100).toString(),
-//     fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))]
-//   };
-// }
